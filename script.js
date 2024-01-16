8a2deacf7866c1c374d05934b3065cfc9affd068
Lepozt <153694960+Lepozt@users.noreply.github.com>
2024-01-16 13:32:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function aboutMe() {
     window.location.href = "aboutme/about.html";
 }
+
+function instaGram() {
+    window.location.href = "//www.instagram.com/lepozt?igsh=YzljYTk1ODg3Zg==" ;
+}
+
+function faceBook() {
+    window.location.href = "https://www.facebook.com/profile.php?id=100093676360603" ;
+}
+
+function gitHub() {
+    window.location.href = "https://github.com/Lepozt" ;
+}
